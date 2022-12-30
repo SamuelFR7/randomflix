12fc6f680c4c29f7d3787f016f412add6aab3d89
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-29 23:37:34 -0300

    feat: loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,42 @@ import { Title } from '../components/Title'
 import { Logo } from '../components/Logo'
 import { useState } from 'react'
 import Movie, { MovieProps } from '../components/Movie'
+import { Loader } from '../components/Loader'
+import classNames from 'classnames'
 
 export default function Homepage() {
   const [movie, setMovie] = useState<MovieProps>()
+  const [loading, setLoading] = useState(false)
 
   async function handleGetMovie() {
+    setLoading(true)
     const getMovie = await fetch(`http://localhost:3000/api/movie`)
       .then((response) => response.json())
       .then((data: MovieProps) => data)
 
     setMovie(getMovie)
+    setLoading(false)
   }
 
   return (
     <div className="flex flex-col items-center mt-[126px] gap-[30px]">
       <Title />
-      {movie && (
+      {!loading && movie ? (
         <Movie
           title={movie.title}
           overview={movie.overview}
           poster_path={movie.poster_path}
         />
-      )}
+      ) : null}
+      {loading && <Loader stroke={'#fff'} width="64" height="64" />}
       <button
         onClick={handleGetMovie}
-        className="flex bg-button text-[12px] text-black px-4 py-3 items-center font-bold gap-[17px] rounded-md hover:bg-subtitle"
+        className={classNames(
+          'flex items-center justify-center bg-button h-[40px] w-[168px]',
+          'text-[12px] text-black px-4 py-3 font-bold gap-[17px] rounded-md ',
+          'hover:bg-subtitle disabled:cursor-not-allowed',
+        )}
+        disabled={loading}
       >
         <Logo width={36} height={25} />
         <span>Encontrar filme</span>

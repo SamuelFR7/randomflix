1db0dbe0e4d8de0e1d70aec2c2e1c0235e764322
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-30 00:34:14 -0300

    fix: head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>RandomFlix</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
